9cdc607d6d55e8f25d0097a97fd8ec2b7f147573
sonaliB15 <sonalibedade15@gmail.com>
2025-08-30 20:14:24 -0700

    Requirements, ingestion, chunking and embeddings

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "virtual_doc",
+    version= "0.1.0",
+    author= "Sonali",
+    packages= find_packages(),
+    install_requires= [],
+)
