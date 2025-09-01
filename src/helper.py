7a2e1afc1952579ef46fcbfb0d9718d734130374
sonaliB15 <sonalibedade15@gmail.com>
2025-08-31 18:00:42 -0700

    RAG pipeline and UI

---
+++ src/helper.py
@@ -0,0 +1,41 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import List
+from langchain.schema import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+import torch
+from langchain_groq import ChatGroq
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+def load_file(path):
+    loader = DirectoryLoader(path, glob= '*.pdf', loader_cls= PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+def filtering(documents):
+    docs: List[Document] = []
+    for doc in documents:
+        src= doc.metadata.get("source")
+        docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata={"source":src})
+        )
+    return docs
+
+def chunking(docs):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size= 1000, chunk_overlap= 200, length_function= len
+    )
+    text = splitter.split_documents(docs)
+    return text
+
+def download_embeddings():
+    embeddings= HuggingFaceEmbeddings(
+        model_name= "BAAI/bge-small-en-v1.5",
+        model_kwargs= {"device":"cuda" if torch.cuda.is_available() else "cpu"}
+    )
+    return embeddings
+
---
+++ store_index.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_file, filtering, chunking, download_embeddings
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+file_path= "./data"
+data = load_file(file_path)
+docs = filtering(data)
+chunks = chunking(docs)
+embeddings = download_embeddings()
+
+pinecone_api_key = PINECONE_API_KEY
+pc = Pinecone(api_key=pinecone_api_key)
+
+index_name = "virtual-doc"
+if not pc.has_index(index_name):
+    pc.create_index(name=index_name, dimension=384, metric="cosine", spec=ServerlessSpec(cloud="aws", region="us-east-1"))
+index = pc.Index(index_name)
+
+docsearch = PineconeVectorStore.from_documents(documents=chunks,embedding=embeddings,index_name=index_name)
