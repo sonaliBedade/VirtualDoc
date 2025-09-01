4e73b3b99396810098e02bc243318e7a9cbb3943
sonaliB15 <sonalibedade15@gmail.com>
2025-09-01 00:11:33 -0700

    ci/cd workflow

--- app.py
+++ app.py
@@ -1,58 +1,46 @@
-from flask import Flask, render_template, jsonify, request
-from src.helper import download_embeddings
-from langchain_pinecone import PineconeVectorStore
-from langchain_groq import ChatGroq
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
-from src.prompt import *
+from flask import Flask, render_template, jsonify, request 
+from src.helper import download_embeddings 
+from langchain_pinecone import PineconeVectorStore 
+from langchain_groq import ChatGroq 
+from langchain.chains import create_retrieval_chain 
+from langchain.chains.combine_documents import create_stuff_documents_chain 
+from langchain_core.prompts import ChatPromptTemplate 
+from dotenv import load_dotenv 
+from src.prompt import * 
 import os
 
-app = Flask(__name__)
+app = Flask(__name__) 
+load_dotenv() 
 
-load_dotenv()
-
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY') 
 GROQ_API_KEY=os.environ.get('GROQ_API_KEY')
 
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["GROQ_API_KEY"] = GROQ_API_KEY
-
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY 
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY 
 embeddings = download_embeddings()
 
-index_name = "virtual-doc" 
-# Embed each chunk and upsert the embeddings into your Pinecone index.
-docsearch = PineconeVectorStore.from_existing_index(
-    index_name=index_name,
-    embedding=embeddings
-)
+index_name = "virtual-doc" # Embed each chunk and upsert the embeddings into your Pinecone index. 
+docsearch = PineconeVectorStore.from_existing_index( index_name=index_name, embedding=embeddings )
 
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3}) 
+chatModel = ChatGroq(model="llama-3.3-70b-versatile") 
+prompt = ChatPromptTemplate.from_messages( [ ("system", system_prompt), ("human", "{input}"), ] )
 
-chatModel = ChatGroq(model="llama-3.3-70b-versatile")
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+qa_chain = create_stuff_documents_chain(chatModel, prompt) 
+rag_chain = create_retrieval_chain(retriever, qa_chain) 
 
-qa_chain = create_stuff_documents_chain(chatModel, prompt)
-rag_chain = create_retrieval_chain(retriever, qa_chain)
-
-@app.route("/")
-def index():
+@app.route("/") 
+def index(): 
     return render_template('chat.html')
 
-@app.route("/get", methods=["GET", "POST"])
-def chat():
-    msg = request.form["msg"]
-    input = msg
-    print(input)
-    response = rag_chain.invoke({"input": msg})
-    print("Response : ", response["answer"])
+@app.route("/get", methods=["GET", "POST"]) 
+def chat(): 
+    msg = request.form["msg"] 
+    input = msg 
+    print(input) 
+    response = rag_chain.invoke({"input": msg}) 
+    print("Response : ", response["answer"]) 
     return str(response["answer"])
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     app.run(host="0.0.0.0", port= 8080, debug= True)
